352a6661ab9a626c2a7b3abf07d31b77ff599a4a
Beatriz <143643089+Dafylin@users.noreply.github.com>
2024-08-01 15:51:35 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação esta se conectando ao banco de dados")
+// Mais um console log de teste:
+console.log ("O Paulo fez esse comit sem você vê na aula!")
+console.log("Enviando para o GiTHub")
